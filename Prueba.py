680937e23de4206b641877b2b069af1b947d0f95
JoseMan72 <josemanuelbonillagarcia@gmail.com>
2023-10-06 13:20:03 +0200

    Prueba de datos del json

--- Prueba.py
+++ Prueba.py
@@ -2,4 +2,8 @@ import json
 import requests
 
 r = requests.get('https://www.amiiboapi.com/api/amiibo/?amiiboSeries=Super%20Mario%20Bros.')
-print(r.text)
+#print(r.text)
+data = r.json()
+#print para ver el nombre de los todos los amiibos
+for i in data['amiibo']:
+   print(i['name'])
