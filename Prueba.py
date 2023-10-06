d8e707953687be8557be8868b0adaaf8ca58c146
JoseMan72 <josemanuelbonillagarcia@gmail.com>
2023-10-06 13:26:18 +0200

    Contador

--- Prueba.py
+++ Prueba.py
@@ -4,6 +4,9 @@ import requests
 r = requests.get('https://www.amiiboapi.com/api/amiibo/?amiiboSeries=Super%20Mario%20Bros.')
 #print(r.text)
 data = r.json()
+cont = 0
 #print para ver el nombre de los todos los amiibos
 for i in data['amiibo']:
    print(i['name'])
+   cont+=1
+print("Total de amiibos: ",cont)
