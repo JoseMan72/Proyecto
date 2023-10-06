35f09e130879faed5690007171e6e89797582373
JoseMan72 <josemanuelbonillagarcia@gmail.com>
2023-10-06 14:46:09 +0200

    Modificación función BuscarPorNombre

--- Prueba.py
+++ Prueba.py
@@ -5,39 +5,40 @@ r = requests.get(
    "https://www.amiiboapi.com/api/amiibo/?amiiboSeries=Super%20Mario%20Bros."
 )
 # print(r.text)
-data = r.json()
-# print para ver el nombre de los todos los amiibos
-
+datos = r.json()
 
 def mostrarListaNombres():
    cont = 0
-   for i in data["amiibo"]:
+   for i in datos["amiibo"]:
       print(i["name"])
       cont += 1
    print("Total de amiibos: ", cont)
 
 
-def buscarNombre():
+def buscarPorNombre():
    nombre = input("Ingrese el nombre del amiibo: ")
-   encontrado = False
-   for i in data["amiibo"]:
-      while encontrado == False:
-            if i["name"] == nombre:
-               print("Nombre: ", i["name"])
-               print("Serie: ", i["amiiboSeries"])
-               print("Tipo: ", i["type"])
-               encontrado = True
-   if encontrado == False:
-      print("No se encontró el amiibo")
+   cont = 0
+   for i in datos["amiibo"]:
+      if i["name"] == nombre:
+         print("Nombre: ", i["name"])
+         print("Serie: ", i["amiiboSeries"])
+         print("Fecha de lanzamiento: ", i["release"]["eu"])
+         cont += 1
+   if cont == 0:
+      print("No se encontró ningún amiibo con ese nombre")
+   print("\nTotal de amiibos: ", cont, "\n")
 
 
 # print del menú que hace llamadas a las funciones
-print("----------Menú----------")
-print("1. Buscar amiibo por nombre")
-print("2. Buscar amiibo por serie")
-print("-------------------------")
-opcion = int(input("Ingrese una opción: "))
-if opcion == 1:
-   mostrarListaNombres()
-if opcion == 2:
-   buscarNombre()
+opcion = 1 #inicializar variable para que entre al while
+while opcion != 0:
+   print("----------Menú----------")
+   print("1. Mostrar lista de nombres")
+   print("2. Buscar amiibos por nombre")
+   print("0. Salir")
+   print("-------------------------")
+   opcion = int(input("Ingrese una opción: "))
+   if opcion == 1:
+      mostrarListaNombres()
+   if opcion == 2:
+      buscarPorNombre()
